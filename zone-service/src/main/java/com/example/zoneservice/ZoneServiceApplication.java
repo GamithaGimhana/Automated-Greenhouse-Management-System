95380e051997cc3986a0e1f7c7d18d568df80cf1
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-04-06 19:38:39 +0530

    Add zone-service module with initial setup, configuration, and application files

---
+++ zone-service/src/main/java/com/example/zoneservice/ZoneServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.zoneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class ZoneServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoneServiceApplication.class, args);
+    }
+
+}
