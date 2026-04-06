875d939081f460023ded0c3148767d5ddc6040f5
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-04-06 19:44:54 +0530

    Add Zone entity with attributes for temperature and device management

---
+++ zone-service/src/main/java/com/example/zoneservice/entity/Zone.java
@@ -0,0 +1,19 @@
+package com.example.zoneservice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Zone {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private double minTemp;
+    private double maxTemp;
+
+    private String deviceId;
+}
