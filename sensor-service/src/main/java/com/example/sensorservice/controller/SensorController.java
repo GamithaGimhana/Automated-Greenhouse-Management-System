28cdd08617a7ba18936f7f3f2fe7711dc95c8dc8
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-04-07 04:58:38 +0530

    Add SensorController for retrieving the latest sensor data

---
+++ sensor-service/src/main/java/com/example/sensorservice/controller/SensorController.java
@@ -0,0 +1,18 @@
+package com.example.sensorservice.controller;
+
+import com.example.sensorservice.dto.SensorDataDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sensors")
+public class SensorController {
+
+    private SensorDataDTO lastData;
+
+    @GetMapping("/latest")
+    public SensorDataDTO getLatest() {
+        return lastData;
+    }
+}
