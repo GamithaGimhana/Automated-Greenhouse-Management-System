1e4768d7aa4655dfb0271d74ccf3aee170a9bfc7
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-04-07 05:32:30 +0530

    Add ZoneClient interface for communicating with zone-service

---
+++ automation-service/src/main/java/com/example/automationservice/client/ZoneClient.java
@@ -0,0 +1,11 @@
+package com.example.automationservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "zone-service")
+public interface ZoneClient {
+
+    @GetMapping("/api/zones/{id}")
+    Object getZone(@PathVariable("id") Long id);
+}
