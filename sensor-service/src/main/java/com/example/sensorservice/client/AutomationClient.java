e39d39013e5fa5125f077ba660c9930d230f4e41
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-04-08 20:17:38 +0530

    Update AutomationClient to use dynamic automation service URL for processing

--- sensor-service/src/main/java/com/example/sensorservice/client/AutomationClient.java
+++ sensor-service/src/main/java/com/example/sensorservice/client/AutomationClient.java
@@ -1,19 +1,18 @@
 package com.example.sensorservice.client;
 
 import com.example.sensorservice.dto.SensorDataDTO;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-@RequiredArgsConstructor
 public class AutomationClient {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     public void sendToAutomation(SensorDataDTO data) {
+
         restTemplate.postForObject(
-                "http://localhost:8083/api/automation/process",
+                "http://AUTOMATION-SERVICE/api/automation/process",
                 data,
                 Void.class
         );
